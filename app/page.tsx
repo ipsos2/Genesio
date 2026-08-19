4a1b7a5d189d77fb67f368e1130f24271f97b707
ipsos2 <123572214+ipsos2@users.noreply.github.com>
2026-08-19 19:08:43 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { fetchNewsItems, fetchCalendarEvents } from "@/lib/data";
 import { HomeContent } from "@/components/home-content";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const [news, events] = await Promise.all([fetchNewsItems(), fetchCalendarEvents()]);
   return <HomeContent news={news.slice(0, 5)} events={events.slice(0, 5)} />;
