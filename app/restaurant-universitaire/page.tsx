2ca291e010e720d3858f6849830339eb84158c01
ipsos2 <123572214+ipsos2@users.noreply.github.com>
2026-09-09 11:30:38 +0000

    Update page.tsx

--- app/restaurant-universitaire/page.tsx
+++ app/restaurant-universitaire/page.tsx
@@ -1,1 +1,8 @@
+import { fetchCrouMenu } from "@/lib/data";
+import { MenuContent } from "@/components/menu-content";
+
+export default async function RestaurantPage() {
+  const items = await fetchCrouMenu();
+  return <MenuContent items={items} />;
+}
 
