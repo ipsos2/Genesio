ff5d13c5329abc4b9be5478225b3af9119386ddb
ipsos2 <123572214+ipsos2@users.noreply.github.com>
2026-09-09 11:34:13 +0000

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,8 +13,10 @@ const links = [
   { href: "/ufr-sciences-de-la-sante", label: "UFR Sciences de la Santé" },
   { href: "/ephr-san-pedro", label: "EPHR de San Pedro" },
   { href: "/calendrier-universitaire", label: "Calendrier Universitaire" },
+  { href: "/restaurant-universitaire", label: "Restaurant U" },
   { href: "/faq", label: "FAQ" },
 ];
+
 
 
 
